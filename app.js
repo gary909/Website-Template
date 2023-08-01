ec39f33d7a99e3fc6bb37c7c962bebb09418e8b5
gary909 <25279957+gary909@users.noreply.github.com>
2023-08-01 19:07:08 +0200

    slider text

--- app.js
+++ app.js
@@ -26,3 +26,22 @@ function toggleFunction() {
         x.className = x.className.replace(" w3-show", "");
     }
 }
+
+
+
+// ++++++++ START OF TEXT SLIDESHOW CODE ++++++++
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 6000); // Change image every 6 seconds
+}
+// ++++++++ END OF TEXT SLIDESHOW CODE ++++++++
