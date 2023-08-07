3be16e9fa4526467d2e544bf22dc402f4f0b67e6
gary909 <25279957+gary909@users.noreply.github.com>
2023-08-07 10:57:36 +0200

    working collapsible

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+// START OF COLLAPSABLE CERTS JS
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
+// END OF COLLAPSABLE CERTS JS
+
+
 // Modal Image Gallery
 function onClick(element) {
   document.getElementById("img01").src = element.src;
